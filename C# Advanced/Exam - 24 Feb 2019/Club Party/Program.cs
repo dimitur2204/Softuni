a44dafdb88e3f2642bdfb5708d7b0abb3e48b2c6
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2020-06-25 23:00:59 +0300

    Add C# Advanced exam practices

---
+++ C# Advanced/Exam - 24 Feb 2019/Club Party/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Club_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hallMaxCap = int.Parse(Console.ReadLine());
+            var halls = new Queue<Tuple<char,int>>();
+            var input = new Stack<char>(Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse));
+            while (input.Any())
+            {
+                var item = input.Pop();
+                if (char.IsDigit(item))
+                {
+                    var value = int.Parse(item.ToString());
+                    var hallValue = halls.Peek().Item2;
+                    if (hallValue + value <= hallMaxCap)
+                    {
+                        halls.Peek().Item2 += value;
+                    }
+                }
+                else
+                {
+                    halls.Enqueue(new Tuple<char, int>(item,0));
+                }
+            }
+        }
+    }
+}
